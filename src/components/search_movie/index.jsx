d33cb4f0dda66cde147b23e8751dbe732cdfc32b
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-10-18 11:46:59 +1000

    refactor: update search movies

--- src/components/search_movie/index.jsx
+++ src/components/search_movie/index.jsx
@@ -1,8 +1,7 @@
 import { useContext, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
-import { Box, IconButton } from "@mui/material";
+import { Box, TextField, IconButton } from "@mui/material";
 import { ColorThemeContext } from "../../theme/toggle_theme";
 import { setSearchMovie, searchMovie } from "../../slices/movies_slice";
 
@@ -11,14 +10,19 @@ const MovieSearchInput = () => {
   const search = useSelector((state) => state.movies.search);
   const dispatch = useDispatch();
 
-  const handleInputChange = (event) => {
-    dispatch(setSearchMovie(event.target.value));
-  };
+  const handleInputChange = useCallback(
+    (event) => {
+      dispatch(setSearchMovie(event.target.value));
+    },
+    [dispatch]
+  );
 
   const handleSearch = useCallback(
     (event) => {
-      dispatch(searchMovie(search));
-      dispatch(setSearchMovie(""));
+      if (search.trim() !== "") {
+        dispatch(searchMovie(search));
+        dispatch(setSearchMovie(""));
+      }
       event.preventDefault();
     },
     [dispatch, search]
